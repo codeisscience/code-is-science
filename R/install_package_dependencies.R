eadd5baf88ac4209b18bb2c80c7d072d0bc43792
Hao Ye <lhopitalified@gmail.com>
2018-05-11 15:24:44 -0400

    adding CRAN mirror spec to install packages script

--- R/install_package_dependencies.R
+++ R/install_package_dependencies.R
@@ -1,1 +1,2 @@
-install.packages(c("googlesheets", "dplyr", "lubridate", "DT", "rmarkdown", "here"))
+install.packages(c("googlesheets", "dplyr", "lubridate", "DT", "rmarkdown", "here"), 
+                 repos = "http://cran.us.r-project.org")
