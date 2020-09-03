b4ee65ff6996e152bb4798fdf077c653beef3726
Raniere Silva <raniere@rgaiacs.com>
2020-09-03 12:35:03 -0300

    Change render script to output full HTML

--- R/render_html.R
+++ R/render_html.R
@@ -1,6 +1,5 @@
 rmarkdown::render(
         here::here("R", "journal_table.Rmd"),
-        output_format = "html_fragment",
         output_file = "journal_table.md",
         output_dir = here::here("content")
 )
