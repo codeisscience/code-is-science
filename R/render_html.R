26ae88cb7ff96fd797b9fc66253d7dd8195359c7
Raniere Silva <raniere@rgaiacs.com>
2020-09-03 12:16:15 -0300

    Improve render script

--- R/render_html.R
+++ R/render_html.R
@@ -1,3 +1,6 @@
-rmarkdown::render(here::here("R", "journal_table.Rmd"))
-file.rename(here::here("R", "journal_table.html"), 
-            here::here("static", "journal_table.html"))
+rmarkdown::render(
+        here::here("R", "journal_table.Rmd"),
+        output_format = "html_fragment",
+        output_file = "journal_table.md",
+        output_dir = here::here("content")
+)
